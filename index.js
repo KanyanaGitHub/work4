b5b503993007308734144c6572f0786ea960b63c
Kanyana <ktoadithep@gmail.com>
2022-12-20 20:35:30 +0700

    addAllw3

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ app.get('/text.css',(req,res) =>{
 app.get('/animated.css',(req,res) =>{
     res.sendFile(dirname+"/work3/animated.css")
 })
+app.get('/charactorPV',(req,res) =>{
+    res.sendFile(dirname+"/work3/page1.html")
+})
+app.get('/about',(req,res) =>{
+    res.sendFile(dirname+"/work3/page2.html")
+})
+app.get('/01.png',(req,res) =>{
+    res.sendFile(dirname+"/work3/01.png")
+})
+app.get('/02.png',(req,res) =>{
+    res.sendFile(dirname+"/work3/02.png")
+})
+app.get('/03.png',(req,res) =>{
+    res.sendFile(dirname+"/work3/03.png")
+})
+app.get('/04.png',(req,res) =>{
+    res.sendFile(dirname+"/work3/04.png")
+})
+
 
 app.get('*',(req,res) =>{
     res.sendFile(__dirname+"/404NF.png")
