24ac512524b25e10ffb00ec56903418ffe51bd43
Kanyana <ktoadithep@gmail.com>
2022-12-20 20:25:05 +0700

    add

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express()
 app.get('/home',(req,res) =>{
     res.sendFile(dirname+"/work3/work3.html")
 })
+app.get('/text.css',(req,res) =>{
+    res.sendFile(dirname+"/work3/text.css")
+})
+app.get('/animated.css',(req,res) =>{
+    res.sendFile(dirname+"/work3/animated.css")
+})
 
 app.listen(3000,()=> {
     console.log('start sever port[3000]')
